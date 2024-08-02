96e67256dcc5861e0b2aa4e79c5e979989398174
Christian Irvine <christian.irvine04@gmail.com>
2024-08-03 11:48:03 +1200

    create GameManager.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameManager Instance;
+
+    private int points = 0;
+    public int Points { get => points; set => points = value; }
+
+    private void Awake()
+    {
+        if (Instance == null && Instance != this)
+        {
+            Destroy(Instance);
+            return;
+        } 
+
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        
+    }
+}
