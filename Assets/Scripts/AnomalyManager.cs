232bbf066bac31fb18d55ba96e96d1eaff7bc6b8
KeyraWebb <keyrajrwebb@gmail.com>
2024-08-03 13:20:25 +1200

    anomaly manager now 'spawns' objects into scene

--- Assets/Scripts/AnomalyManager.cs
+++ Assets/Scripts/AnomalyManager.cs
@@ -1,4 +1,7 @@
 using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AnomalyManager : MonoBehaviour
@@ -27,8 +30,17 @@ public class AnomalyManager : MonoBehaviour
     //pick an anomoly and spawn 
     private void SpawnAnomaly()
     {
-        Debug.Log("spawned a thing");
         spawnCooldown = Random.Range(minTimeBetweenSpawn, maxTimeBetweenSpawn);
         lastSpawnTime = Time.time;
+        int selection = Random.Range(0, anomalies.Count - 1);
+        if(anomalies.Count != 0){
+            anomalies[selection].gameObject.SetActive(true);
+            anomalies.RemoveAt(selection);
+            Debug.Log("spawned a thing");
+        }
+        else
+        {
+            Debug.Log("nothing to spawn");
+        }
     }
 }
