4dfe0b46e6cb9589e872c4778de70bf5246d764c
otherotherjosh <itsotherjosh@gmail.com>
2024-08-04 18:39:42 +1200

    gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,7 +14,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        if (Instance == null && Instance != this)
+        if (Instance != null && Instance != this)
         {
             Destroy(Instance);
             return;
